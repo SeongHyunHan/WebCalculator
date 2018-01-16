2c703d1fb1d164580eb48c9ce2aa98cf3602545a
Seong Hyun Han <seonghyunhan7193@gmail.com>
2018-01-16 16:52:23 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+//import modules
+const http = require('http');
+const express = require('express');
+
+//import custom modules
+const Calc = require('./modules/Calc');
+const Log = require('./modules/Log');
---
+++ modules/Calc.js
@@ -0,0 +1,3 @@
+var Calc = {};
+
+module.exports = Calc;
