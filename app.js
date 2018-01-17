a12f571fae1222f0ded65b20fbc2aa1ca34ef196
Seong Hyun Han <seonghyunhan7193@gmail.com>
2018-01-17 14:15:06 -0500

    Implement Router, Add Simple Calcualtion Router

--- app.js
+++ app.js
@@ -30,18 +30,55 @@ app.use((static(path.join(__dirname, 'public'))));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-// Middleware parse message and send to page according to value stored in button
-app.use((req, res, next) => {
+const router = new express.Router();
+
+// Router parse message and send to page according to value stored in button
+router.route('/process/pageSelect').post((req, res) => {
+    console.log("/process/pageSelected Router Called");
     var paramValue = req.body.page || req.query.page // Post Method || Get Method
     switch(paramValue){
         case 'BMI Calculator':
-            res.redirect('bmi.html');
+            res.redirect('../bmi.html');
             break;
         case 'Unit Convertor':
-            res.redirect('unit.html');
+            res.redirect('../unit.html');
             break;
         case 'Simple Calculator':
-            res.redirect('simple.html');
+            res.redirect('../simple.html');
             break;
     }
 });
+
+router.route('/process/simple').post((req, res) => {
+    console.log('/process/simple Router Called');
+    var num1 = req.body.num1;
+    var operation = req.body.operation;
+    var num2 = req.body.num2;
+    var result;
+    switch(operation){
+        case "+":
+            result = Calc.add(num1, num2);
+            break;
+        case "-":
+            result = Calc.sub(num1, num2);
+            break;
+        case "*":
+            result = Calc.mul(num1, num2);
+            break;
+        case "/":
+            result = Calc.div(num1, num2);
+            break;
+    }
+    res.send(result);
+});
+
+router.route('/process/bmi').post((req, res) => {
+    console.log('/process/bmi Router Called');
+});
+
+router.route('/process/unit').post((req, res) => {
+    console.log('/process/unit Router Called');
+});
+
+// Register router to Middleware
+app.use('/', router);
