63800bd80646360e49dd281326e19c8b9ad7f8f9
Seong Hyun Han <seonghyunhan7193@gmail.com>
2018-01-17 14:13:27 -0500

    Modified Calc.js\

--- modules/Calc.js
+++ modules/Calc.js
@@ -1,3 +1,41 @@
 var Calc = {};
 
+var result;
+
+Calc.add = (num1, num2) => {
+    if(isNaN(num1) || isNaN(num2)){
+        return "Not a Number!";
+    }
+    result = parseFloat(num1)+parseFloat(num2);
+    return num1 + " + " + num2 + " = " + result;
+};
+
+Calc.sub = (num1, num2) => {
+    if(isNaN(num1) || isNaN(num2)){
+        return "Not a Number!";
+    }
+    result = parseFloat(num1)-parseFloat(num2);
+    return num1 + " - " + num2 + " = " + result;
+};
+
+Calc.mul = (num1, num2) => {
+    if(isNaN(num1) || isNaN(num2)){
+        return "Not a Number!";
+    }
+    result = parseFloat(num1)*parseFloat(num2);
+    return num1 + " * " + num2 + " = " + result;
+};
+
+Calc.div = (num1, num2) => {
+    if(isNaN(num1) || isNaN(num2)){
+        return "Not a Number!";
+    }
+    if(num2 == 0){
+        return "It can not divide with 0";
+    }else{
+        result = parseFloat(num1)/parseFloat(num2);
+        return num1 + " / " + num2 + " = " +  result;
+    }
+}
+
 module.exports = Calc;
